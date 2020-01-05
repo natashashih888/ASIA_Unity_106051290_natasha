8bd5ae60a4a1973801bca26e761c3d2fb720781f
natashashih888 <55772622+natashashih888@users.noreply.github.com>
2020-01-06 03:49:57 +0800

    0106

--- Unity_106051290_natasha/Assets/C/eat.cs
+++ Unity_106051290_natasha/Assets/C/eat.cs
@@ -1,14 +1,25 @@
 ﻿using UnityEngine;
-
+using UnityEngine.UI;
 public class eat : MonoBehaviour
+    
 {
+    [Header("角錐數量")]
+    public Text textCount;
+    [Header("勝利畫面")]
+    public GameObject ObjectTip;
+
+    public int propCount;
+    public int propTotal = 10;
+
     private void OnCollisionEnter(Collision collision)
     {
-      if (collision.gameObject.tag=="角錐")
-
-        {
+      if (collision.gameObject.tag == "角錐")
+      {
             Destroy(collision.gameObject);
-        }
+
+            propCount++;
+            textCount.text ="角錐:"+propCount+"獲得   個角椎";
+      }
        
     }
 }

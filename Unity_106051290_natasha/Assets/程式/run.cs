04d0b33e0062e780d0da5f9adc8a436512d96d99
natashashih888 <55772622+natashashih888@users.noreply.github.com>
2020-01-05 16:49:06 +0800

    0105

--- Unity_106051290_natasha/Assets/程式/run.cs
+++ Unity_106051290_natasha/Assets/程式/run.cs
@@ -1,19 +1,19 @@
 ﻿using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class run : MonoBehaviour
 {
     public Animator ani;
     private void Update()
     {
-        run();
+        Run();
     }
-    private void run()
+    private void Run()
     {
         //print("水平" + Input.GetAxis("horizontal"));
         //print("垂直" + Input.GetAxis("vertical"));
 
-        float h = Input.GetAxis("horizontal");
-        float v = Input.GetAxis("vertical");
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
 
         ani.SetBool("跑步", h != 0 || v != 0);
     }
